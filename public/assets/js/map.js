d6b895194207736a225c52b9f60cf8241f1b40a7
Qamar Stationwala <YoPappy0@gmail.com>
2018-01-13 04:07:44 -0800

    user can now get directions even if geolocation fails, backup opens new windows at maps.google.com

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -23,31 +23,47 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay, startCoo
     }, function (response, status) {
         if (status === 'OK') {
             directionsDisplay.setDirections(response);
+            displayDirections(response.routes[0].legs[0]);
         } else {
             window.alert('Directions request failed due to ' + status);
+            backupDirs()
         }
     });
 }
+
+function backupDirs() {
+    window.open('https://www.google.com/maps/dir/?api=1&destination=team3j&destination_place_id=EiYyNDI0IEUgQmFsbCBSZCwgQW5haGVpbSwgQ0EgOTI4MDYsIFVTQQ')
+}
+
+function displayDirections(steps) {
+    $("#dirpanel").empty();
+    var newUl = $("<ul class='list-group'>");
+    newUl.append("<li class='list-group-item'>Starting from: " + steps.start_address + "</li>");
+    newUl.append("<li class='list-group-item'>Distance: " + steps.distance.text + ", Duration: " + steps.duration.text + "</li>");
+    for (var i = 0; i < steps.steps.length; i++) {
+        newUl.append("<li class='list-group-item'>" + (i + 1) + ". " + steps.steps[i].instructions + "</li>");
+    }
+    newUl.append("<li class='list-group-item'>Ending at: " + steps.end_address + "</li>")
+    $("#dirpanel").append(newUl);
+}
 window.onload = function () {
     $("#getDirs").on("click", function () {
-        if (navigator.geolocation) {
+        if (navigator && navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var directionsDisplay = new google.maps.DirectionsRenderer;
             var directionsService = new google.maps.DirectionsService;
             directionsDisplay.setMap(map);
-            directionsDisplay.setPanel(document.getElementById('dirpanel'));
             var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
             calculateAndDisplayRoute(directionsService, directionsDisplay, pos);
-            $("#getDirs").css("display", "none");
         }, function () {
-            alert("Uh-oh! Something went wrong when we tried to get your location.")
+            backupDirs();
             console.error("Uh-oh! Something went wrong when we tried to get your location.");
         });
         } else {
-            alert("Your browser does not support geolocations")
+            backupDirs();
             console.error("Your browser does not support geolocations.");
         }
     });
