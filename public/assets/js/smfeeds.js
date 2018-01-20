888e7bd16767e00ca631e7bc2973d1748815385e
Qamar Stationwala <YoPappy0@gmail.com>
2018-01-20 07:55:22 -0800

    added facebook feed, setup db to only force sync in dev env

--- public/assets/js/smfeeds.js
+++ public/assets/js/smfeeds.js
@@ -1,38 +1,62 @@
 function grabFBFeed() {
-    $("#smFeedBox").empty();
-    $("#smFeedBox").append("<p>Facebook feed under construction</p>")
+    axios
+        .get("/api/facebook")
+        .then(function(response) {
+            $("#fbFeedBox").empty();
+            var newUl = $("<ul class='list-group text-center'>");
+            console.log(response);
+            for (var i = 0; i < response.data.data.length; i++) {
+                var newPost = response.data.data[i].message || response.data.data[i].story
+                newUl.append("<li class='list-group-item'>" + newPost + "</li>")
+            }
+            $("#fbFeedBox").append(newUl);
+        })
+        .catch(function(error) {
+            console.error(error);
+        });
 }
 
 function grabTweets() {
-    $.get("/api/twitter", function(data) {
-        $("#smFeedBox").empty();
-        var newUl = $("<ul class='list-group text-center'>");
-        for (var i = 0; i < data.length; i++) {
-            newUl.append("<li class='list-group-item'>" + data[i].text + "</li>")
-        }
-        $("#smFeedBox").append(newUl);
-    })
+    axios
+        .get("/api/twitter")
+        .then(function(response) {
+            $("#twitFeedBox").empty();
+            var newUl = $("<ul class='list-group text-center'>");
+            for (var i = 0; i < response.data.length; i++) {
+                newUl.append("<li class='list-group-item'>" + response.data[i].text + "</li>")
+            }
+            $("#twitFeedBox").append(newUl);
+        })
+        .catch(function(error) {
+            console.error(error);
+        });
 }
 
 function grabInstaFeed() {
-    $("#smFeedBox").empty();
-    $("#smFeedBox").append("<p>Instagram feed under construction</p>")
+    $("#instaFeedBox").empty();
+    $("#instaFeedBox").append("<p>Instagram feed under construction</p>")
 }
 
 window.onload = function() {
     grabFBFeed();
-
+    grabTweets();
+    grabInstaFeed();
+    $("#fbFeedBox").css("display", "block");
+    $("#fbFeed").css("display", "block");
     $("#feedTabs > li").on("click", function () {
         $("#feedTabs > li").removeClass("active");
         $(this).addClass("active");
     });
     $("#fbFeed").on("click", function() {
-        grabFBFeed();
+        $(".feedBox").css("display", "none");
+        $("#fbFeedBox").css("display", "block");
     });
     $("#twitFeed").on("click", function() {
-        grabTweets();
+        $(".feedBox").css("display", "none");
+        $("#twitFeedBox").css("display", "block");
     });
     $("#instaFeed").on("click", function() {
-        grabInstaFeed();
+        $(".feedBox").css("display", "none");
+        $("#instaFeedBox").css("display", "block");
     });
 }
